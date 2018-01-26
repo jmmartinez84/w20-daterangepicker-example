9589f6ea6ddcfc195da206df6c04c49ccbc43aa0
JOSE MANUEL MARTINEZ LAINEZ - E437892 <E437892@inetpsa.com>
2018-01-26 13:09:05 +0100

    First commit

---
+++ example/modules/content.js
@@ -0,0 +1,28 @@
+define([
+    '{angular}/angular',
+    '{angular-resource}/angular-resource',
+    '{angular-daterangepicker}/js/angular-daterangepicker',
+    "[css]!{bootstrap-daterangepicker}/daterangepicker.css"
+], function(angular) {
+    'use strict';
+
+	var module = angular.module('content', ['ngResource','daterangepicker']);
+
+   
+
+	module.controller('ContentController', [ '$scope', '$timeout',  function($scope) {
+        $scope.datePicker = {
+            date:{
+                startDate: null, 
+                endDate: null
+            }
+        };
+  
+              
+    }]);
+    
+
+	return {
+		angularModules : [ 'content' ]
+	};
+});
